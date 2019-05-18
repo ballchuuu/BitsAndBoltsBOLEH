09167f2fd4f0d1fd6d64d3e5b9ef9004e0445070
ballchuuu <ballchuuu@gmail.com>
2019-05-18 13:16:02 +0800

    Update models.py

--- web/models.py
+++ web/models.py
@@ -7,29 +7,22 @@ class Location(db.Model):
 	user = db.Column(db.String(500), nullable=False, primary_key=True)
 	x = db.Column(db.Float, nullable=False)
 	y = db.Column(db.Float, nullable=False)
-	time = db.Column(db.DateTime, nullable=False)
+	time = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 	
-	def __init__(self, user, x,y,time):
+	def __init__(self, user, x,y):
 		self.user = user
 		self.x = x
 		self.y = y
-		self.time = 
 
-	# def __repr__(self):
-	# 	return 'Name: {}, NRIC_no: {}'.format(self.name, self.nric_no)
+	def __repr__(self):
+		return 'x: {}, y:{}'.format(self.x, self.y)
 
-	# def serialize(self):
-	# 	result = {
-	# 	'NRIC_no': self.nric_no,
-	# 	'name':self.name,
-	# 	'year_of_birth': self.year_of_birth,
-	# 	'gender': self.gender,
-	# 	'mobile_no':self.mobile_no,
-	# 	'weight': self.weight,
-	# 	'height': self.height,
-	# 	'tracker_type': self.tracker_type,
-	# 	'date_joined': self.date_joined, 
-	# 	'total_healthpoints': self.total_healthpoints
-	# 	}
-	# 	return result 
+	def serialize(self):
+		result = {
+		'user': self.user,
+		'x':self.x,
+		'y': self.y,
+		'time': self.time,
+		}
+		return result 
 		
