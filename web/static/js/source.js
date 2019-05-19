36c02f6fad8c92af47ede104ee697ddb7b05086d
Jane Seah <jane.seah.2016@sis.smu.edu.sg>
2019-05-19 11:27:48 +0800

    Use websocket

--- web/static/js/source.js
+++ web/static/js/source.js
@@ -1,43 +1,47 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('updates', function (data) {
+  var trace1 = {
+    x: data.x,
+    y: data.y,
+    mode: 'markers',
+    marker: {
+      size: data.count
+    },
+    type: 'scatter'
+  };
+
+  Plotly.plot('plotly-div', [trace1], layout);
+});
+
 function plotFloor() {
-  $.getJSON('/api/crowd_report', function(data) {
-    var trace1 = {
-      x: data.x,
-      y: data.y,
-      mode: 'markers',
-      marker: {
-        size: data.count
-      },
-      type: 'scatter'
-    };
-    var layout = {
-      autosize: false,
-      images: [
-        {
-          source: 'https://raw.githubusercontent.com/ballchuuu/BitsAndBoltsBOLEH/master/layouts/COM1_L2_2.jpg?token=AI7O7MA32OC47W44IUC6QB245AH7A',
-          xref: 'x',
-          yref: 'y',
-          x: 0,
-          y: 40,
-          sizex: 40,
-          sizey: 40,
-          sizing: 'contain',
-          opacity: 0.5,
-          layer: 'below'
-        }],
-      width: 1000,
-      height: 1000,
-      xaxis: {
-        range: [0, 40],
-        dtick: 1
-      },
-      yaxis: {
-        range: [0, 40],
-        dtick: 1
-      }
-    };
-  
-    Plotly.newPlot('plotly-div', [trace1], layout);
-  })
+  var layout = {
+    autosize: false,
+    images: [
+      {
+        source: 'https://raw.githubusercontent.com/ballchuuu/BitsAndBoltsBOLEH/master/layouts/COM1_L2_2.jpg?token=AI7O7MA32OC47W44IUC6QB245AH7A',
+        xref: 'x',
+        yref: 'y',
+        x: 0,
+        y: 40,
+        sizex: 40,
+        sizey: 40,
+        sizing: 'contain',
+        opacity: 0.5,
+        layer: 'below'
+      }],
+    width: 1000,
+    height: 1000,
+    xaxis: {
+      range: [0, 40],
+      dtick: 1
+    },
+    yaxis: {
+      range: [0, 40],
+      dtick: 1
+    }
+  };
+
+  Plotly.newPlot('plotly-div', [trace1], layout);
 }
 
 //helping methods
